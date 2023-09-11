e34b5cc05ccaf7a77782c9fb3e2378d5ebd3a706
tzuk999 <tzuk999@gmail.com>
2023-09-11 20:51:08 +0300

    created models and serializers

--- store/models.py
+++ store/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def number_of_products(self):
+        return self.products.count()
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    stock = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product_images/')
+    description = models.TextField()
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, related_name='carts', on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='CartItem')
+
+    def __str__(self):
+        return f"Cart for {self.user.username}"
+    
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in {self.cart}"
---
+++ store/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import *
+
+class CategorySerializer(serializers.ModelSerializer):
+    number_of_products = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = '__all__'
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = '__all__'
+
+
+class CartSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    products = CartItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = '__all__'
