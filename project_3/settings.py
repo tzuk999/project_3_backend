89fb1172b2f793f9d3aef81aea57e5728818fbc5
tzuk999 <tzuk999@gmail.com>
2023-09-12 18:32:35 +0300

    added url to cors

--- project_3/settings.py
+++ project_3/settings.py
@@ -129,6 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
 "http://127.0.0.1:5501",
 "http://127.0.0.1:5500",
+"http://localhost:3000"
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
