f6d68d5f75233b43b6ed8a832c03a4ca9670962e
tzuk999 <tzuk999@gmail.com>
2023-09-15 13:30:14 +0300

    change the product image from url to store an image in the db

--- project_3/settings.py
+++ project_3/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,3 +134,8 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('products/', views.all_products_view, name='all-products'),
     path('categories/', views.all_categories_view, name='all-categories'),
     path('cart/<int:user_id>/', views.cart_by_user_view, name='cart-by-user'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
